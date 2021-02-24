4a707cc74ecfab3e2020b604a1b9f13ceb784725
Janet <janetliu93@gmail.com>
2021-02-24 22:27:11 +0800

    Update codehw3.php

--- codehw3.php
+++ codehw3.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-		<title>Code Homework #2
+		<title>Code Homework #3
 		</title>
 </head>
 <html>
@@ -10,10 +10,11 @@
 echo("<h1>Code Homework #3 [Arrays]</h1>"); 
 
 //Problem #1
-echo( "<h2>Challenge 1: Book List</h2>" . "<br />");
-//Create multi-dimensional array 
+echo("<h2>Challenge 1: Book Lists</h2>" . "<br />");
+
+    //Create a two-dimensional array defined $bookdata that stores rows of data as one array, and each row of book information as an array within this array.
 $bookdata = array(
-	array("Title","First Name","Last Name","Number of Pages","Type","Price"),
+	array("Title","First Name","Last Name","Number of Pages","Type","Price"), #6 elements
 	array("PHP and MySQL Web Development","Luke","Welling",144,"Paperback",31.63), 
 	array("Web Design with HTML, CSS, JavaScript and jQuery","Jon","Duckett",135,"Paperback",41.23),
 	array("PHP Cookbook: Solutions & Examples for PHP Programmers","David","Sklar",14,"Paperback",40.88),
@@ -22,69 +23,69 @@ $bookdata = array(
 	array("Programming PHP","Kevin","Tatroe",26,"Paperback",28.96)
 );
 
-//Plug book data into a table
+//Plug arrays/elements into table. put a nststed for loop to access elements of $bookdata array.
 echo "<table border=\"1\">";
+//express what to use as a counter
 for($i=0;$i<count($bookdata);$i++) {
-  echo('<tr>');
-  for($j=0;$j<count($bookdata[$i]);$j++) {
-	  ($i == 0 ) ? $style = ' style="background-color: gray; color:white; font-style:heading;text-align:center"' : $style = NULL;
-    echo("<td$style>" . $bookdata[$i][$j] . "</td>");
+  echo('<tr>'); #table row 
+#say stop counter when it gets to be less than count function (stop iterating the array)
+  for($b=0;$b<count($bookdata[$i]);$b++) {
+	  ($i == 0 ) ? $style = ' style="background-color: Plum; color:white; font-style:heading;text-align:center"' : $style = NULL;
+    echo("<td$style>" . $bookdata[$i][$b] . "</td>"); #data cell
   } 
   echo('</tr>');
 }
 echo "</table><p></p>";
 
-//calculate total price
-echo "Your total price is $";
+//calculate the total price of index 5
+echo ("Your total price is $");
 echo $bookdata[1][5]+$bookdata[2][5]+$bookdata[3][5]+$bookdata[4][5]+$bookdata[5][5]+$bookdata[6][5];
 
     
 //Problem #2
 echo( "<h2>Challenge 2: Coin Toss, continued</h2>" . "<br />");      
     
-    $a = 4; //set the number of heads you want in a row.
+    $a = 4; //define $a as the desired number "heads in a row" 
 
-//Make a function that will be used in the loop. It will stop the loop once it gets the desired result.
+//Make a function that can be used in a loop. We will stop the loops once we get the desired number of heads.
 function cointoss($toss)
 {
-  $test_array = [];
-  $result_array = [];
+  $toss_array = []; #make an empty array for "toss"
+  $result_array = []; #make an empty array for "result"
   do {
     if (mt_rand(1,2) == 1)
     {
-      array_push($result_array, 1);
-      unset($test_array);
-      $test_array = [];
+      array_push($result_array, 1); #use array_push to insert element 'heads' into existing array
+      unset($toss_array); #delete array 
+      $toss_array = [];
     }
 		else
 		{
-		array_push($result_array, 2);
-		array_push($test_array, 2);
+		array_push($result_array, 2); #use array_push to insert element 'tails' into existing array
+		array_push($toss_array, 2);
 		}
-  } while(count($test_array) < $toss);
+  } while(count($toss_array) < $toss); #break loop once $toss_array reaches $a (desired number of heads)
   return $result_array;
 }
 
 
-echo "We are looking for $a heads in a row...<br>";
+echo ("Beginning the coin flipping, looking for $a heads in a row..." . "<br /><br />");
 $result = cointoss($a);
-$count = count($result);
+$count = count($result); 
 
-// I made a loop that prints the result list until we get the desired result.
+// make a foreach...as loop for $result array that will echo until desire doutcome of 'X' heads, with each loop that will set $flip as value of that position in array. 
 foreach ($result as $flip) {
   if ($flip == 1)
   {
-    echo "<img src='http://lincolnpennies.net/wp-content/uploads/2009/08/lincoln_penny_obverse1-250x249.jpg' alt='Heads' width=50 height-50/>";
+    echo "<img src='http://lincolnpennies.net/wp-content/uploads/2009/08/lincoln_penny_obverse1-250x249.jpg' alt='Heads' width=50 height-50/>" . "\n"; //if flip = 1, echo heads image
   }
   elseif ($flip == 2)
   {
-    echo "<img src='http://lincolnpennies.net/wp-content/uploads/2009/08/lincoln-memorial-penny-250x248.jpg' alt='Tails' width=50 height-50/>";
+    echo "<img src='http://lincolnpennies.net/wp-content/uploads/2009/08/lincoln-memorial-penny-250x248.jpg' alt='Tails' width=50 height-50/>" . "\n"; //if flip = 1, echo tails image
   }
-} echo "<br><p>It took $count tosses to land $a heads in a row.</p>";
+} echo ("<br />". "<br>Flipped $a heads in  arow, in $count flips!");
 echo "</div>";
-
-
-                                    
+                                
 ?>
 
 </body>
